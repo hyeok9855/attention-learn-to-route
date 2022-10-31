733f5dd7b03479011ca19a8ad731eda37cd1b9b1
hyeok9855 <hyeok9855@gmail.com>
2022-10-31 17:23:17 +0900

    Add: Run imitation learning

---
+++ train_il.py
@@ -0,0 +1,157 @@
+import os
+import time
+from functools import partial
+from tqdm import tqdm as _tqdm
+import torch
+import math
+
+from torch.utils.data import DataLoader
+from torch.nn import DataParallel
+
+from nets.attention_model import set_decode_type
+from utils.log_utils import log_values_il
+from utils import move_to
+
+tqdm = partial(_tqdm, dynamic_ncols=True)
+
+
+def get_inner_model(model):
+    return model.module if isinstance(model, DataParallel) else model
+
+
+def validate(model, dataset, opts):
+    # Validate
+    print('Validating...')
+    cost = rollout(model, dataset, opts)
+    avg_cost = cost.mean()
+    print('Validation overall avg_cost: {} +- {}'.format(
+        avg_cost, torch.std(cost) / math.sqrt(len(cost))))
+
+    return avg_cost
+
+
+def rollout(model, dataset, opts):
+    # Put in greedy evaluation mode!
+    set_decode_type(model, "greedy")
+    model.eval()
+
+    def eval_model_bat(bat):
+        with torch.no_grad():
+            cost, _ = model(move_to(bat, opts.device))
+        return cost.data.cpu()
+
+    return torch.cat([
+        eval_model_bat(bat)
+        for bat
+        in tqdm(DataLoader(dataset, batch_size=opts.eval_batch_size), disable=opts.no_progress_bar)
+    ], 0)
+
+
+def clip_grad_norms(param_groups, max_norm=math.inf):
+    """
+    Clips the norms for all param groups to max_norm and returns gradient norms before clipping
+    :param optimizer:
+    :param max_norm:
+    :param gradient_norms_log:
+    :return: grad_norms, clipped_grad_norms: list with (clipped) gradient norms per group
+    """
+    grad_norms = [
+        torch.nn.utils.clip_grad_norm_(
+            group['params'],
+            max_norm if max_norm > 0 else math.inf,  # Inf so no clipping but still call to calc
+            norm_type=2
+        )
+        for group in param_groups
+    ]
+    grad_norms_clipped = [min(g_norm, max_norm) for g_norm in grad_norms] if max_norm > 0 else grad_norms
+    return grad_norms, grad_norms_clipped
+
+
+def train_epoch(model, optimizer, lr_scheduler, epoch, training_dataset, val_dataset, tb_logger, opts):
+    print("Start train epoch {}, lr={} for run {}".format(epoch, optimizer.param_groups[0]['lr'], opts.run_name))
+    step = epoch * (len(training_dataset) // opts.batch_size)
+    start_time = time.time()
+
+    if not opts.no_tensorboard:
+        tb_logger.log_value('learnrate_pg0', optimizer.param_groups[0]['lr'], step)
+
+    # Generate new training data for each epoch
+    training_dataloader = DataLoader(training_dataset, batch_size=opts.batch_size, num_workers=1)
+
+    # Put model in train mode!
+    model.train()
+    set_decode_type(model, "sampling")
+
+    for batch_id, batch in enumerate(tqdm(training_dataloader, disable=opts.no_progress_bar)):
+
+        train_batch(
+            model,
+            optimizer,
+            epoch,
+            batch_id,
+            step,
+            batch,
+            tb_logger,
+            opts
+        )
+
+        step += 1
+
+    epoch_duration = time.time() - start_time
+    print("Finished epoch {}, took {} s".format(epoch, time.strftime('%H:%M:%S', time.gmtime(epoch_duration))))
+
+    if (
+        (opts.checkpoint_epochs != 0 and epoch % opts.checkpoint_epochs == 0)
+        or (opts.save_all_ckpt_from is not None and epoch >= opts.save_all_ckpt_from)
+        or (epoch == opts.n_epochs - 1)
+    ):
+        print('Saving model and state...')
+        torch.save(
+            {
+                'model': get_inner_model(model).state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'rng_state': torch.get_rng_state(),
+                'cuda_rng_state': torch.cuda.get_rng_state_all(),
+            },
+            os.path.join(opts.save_dir, 'epoch-{}.pt'.format(epoch))
+        )
+
+    avg_reward = validate(model, val_dataset, opts)
+
+    if not opts.no_tensorboard:
+        tb_logger.log_value('val_avg_reward', avg_reward, step)
+
+    # lr_scheduler should be called at end of epoch
+    lr_scheduler.step()
+
+
+def train_batch(
+        model,
+        optimizer,
+        epoch,
+        batch_id,
+        step,
+        batch,
+        tb_logger,
+        opts
+):
+    x, pi_expert = batch
+    x = move_to(x, opts.device)
+
+    # Evaluate model, get costs and log probabilities
+    cost, log_likelihood = model(x, pi_expert=pi_expert)
+    
+    # Calculate loss
+    loss = - log_likelihood.mean()
+
+    # Perform backward pass and optimization step
+    optimizer.zero_grad()
+    loss.backward()
+    # Clip gradient norms and get (clipped) gradient norms for logging
+    grad_norms = clip_grad_norms(optimizer.param_groups, opts.max_grad_norm)
+    optimizer.step()
+
+    # Logging
+    if step % int(opts.log_step) == 0:
+        log_values_il(cost, grad_norms, epoch, batch_id, step,
+                      log_likelihood, loss, tb_logger, opts)
